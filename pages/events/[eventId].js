7700035a66a48de32cc1d3095e480b8acb744ef7
Anders Sollander <asolland@mathworks.com>
2022-08-13 19:53:58 +0200

    Grab the event using the helper function

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,15 @@
 import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
 
 function EventDetailPage() {
   const router = useRouter();
+
+  const event = getEventById(router.query.eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
     <div>
       <h1>Event Details Page</h1>
