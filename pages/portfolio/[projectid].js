0fe7318618ed9fb5fb53f483387d69968d573e27
Anders Sollander <asolland@mathworks.com>
2022-08-13 12:46:32 +0200

    More portfolio stuff

---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+
+function PortfolioProjectPage() {
+  const router = useRouter();
+
+  console.log("router.pathname", router.pathname)  
+  console.log("router.query", router.query)  
+  const query = router.query;
+  return (
+    <div>
+      <h1>The portfolio project page for project {query.projectid}</h1>
+    </div>
+  );
+}
+
+export default PortfolioProjectPage;
