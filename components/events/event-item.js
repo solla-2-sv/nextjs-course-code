47b9d66091e46cfd6e55a239c51485f9af5725be
Anders Sollander <asolland@mathworks.com>
2022-08-13 19:53:43 +0200

    Use icons in list

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,9 @@
 import Button from "../ui/button";
 
 import classes from "./event-item.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const example = {
   id: "e3",
@@ -34,18 +37,28 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humandReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-            <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
   );
 }
+
+
+
 
 export default EventItem;
