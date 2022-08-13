01ceb8c65e6f1706cd5964cff791a3f47345b1b1
Anders Sollander <asolland@mathworks.com>
2022-08-13 23:41:02 +0200

    Add results title for search, with back button

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -2,17 +2,12 @@ import { useRouter } from "next/router";
 import { Fragment } from "react";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
+import ResultsTitle from "../../components/results-title/results-title";
 import { getFilteredEvents } from "../../dummy-data";
 
 function FilteredEventPage() {
   const router = useRouter();
-  function findEventsHandler(year, month) {
 
-    const fullPath = `/events/${year}/${month}`;
-    router.push(fullPath);
-  }
-
-  
   const filterData = router.query.slug;
   if (!filterData) {
     return <p className="center">Loading ...</p>;
@@ -35,15 +30,16 @@ function FilteredEventPage() {
     return <p>Invalid filter, please fix your values!</p>;
   }
 
-  const filteredEvents = getFilteredEvents({year: numYear, month: numMonth});
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return <p>No events found for chosen filter.</p>;
   }
 
+  const date = new Date(numYear, numMonth - 1);
   return (
     <Fragment>
-      <EventsSearch onSearch={findEventsHandler} />
+      <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </Fragment>
   );
