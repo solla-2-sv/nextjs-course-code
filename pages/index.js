3596d656d5db47e12edd9cacfcb622acd6ff0537
Anders Sollander <asolland@mathworks.com>
2022-08-13 13:03:29 +0200

    Using Link element

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 function HomePage() {
   return (
     <div>
       <h1>Home page</h1>
+      <ul>
+        {/* Bad idea, as this does a new request to the server, losing app-wide state */}
+        {/* <li>
+            <a href="/portfolio">Portfolio</a>
+        </li> */}
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
