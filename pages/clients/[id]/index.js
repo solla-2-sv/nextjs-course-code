8912e944b8c60262ead28b1b96324268204b1fda
Anders Sollander <asolland@mathworks.com>
2022-08-13 13:20:03 +0200

    Programmatic navigation

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,22 @@ import { useRouter } from "next/router";
 function ClientsProjectPage() {
   const router = useRouter();
 
+  function loadProjectHandler() {
+    //load data ...
+    // router.push('/clients/max/projecta')
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: router.query.id,
+        clientprojectid: "otherproject",
+        queryParam: "gobbledygook",
+      },
+    });
+  }
   return (
     <div>
       <h1>The Clients Projets Page: id == {router.query.id}</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
