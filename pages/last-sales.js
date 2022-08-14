914f32621997e5d5f91de3f4ab72eaf378d33e7f
Anders Sollander <asolland@mathworks.com>
2022-08-14 14:59:33 +0200

    static fetching too

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,49 +1,100 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const firebaseBase =
   "https://react-goofing-around-default-rtdb.firebaseio.com/";
 
-function LastSalesPage() {
-  const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
+  //   const [isLoading, setIsLoading] = useState(false);
+
+  const salesURL = firebaseBase + "sales.json";
+  //   console.log(salesURL);
+  const { data, error } = useSWR(salesURL, (url) =>
+    fetch(url).then((res) => res.json())
+  );
+  //   console.log("data: ", data);
+  //   console.log("error: ", error);
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(firebaseBase + "sales2.json")
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          const transformedSales = [];
-          for (const key in data) {
-            transformedSales.push({
-              id: key,
-              username: data[key].username,
-              volume: data[key].volume,
-            });
-          }
-          setSales(transformedSales);
-        }
-        setIsLoading(false);
-    });
-  }, []);
+    // console.log("useEffect: data: ", data);
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-  if (isLoading) {
-    return <p>Loading ...</p>;
+  //   console.log("data: ", data);
+  //   console.log("error: ", error);
+
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch(salesURL)
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         if (data) {
+  //           const transformedSales = [];
+  //           for (const key in data) {
+  //             transformedSales.push({
+  //               id: key,
+  //               username: data[key].username,
+  //               volume: data[key].volume,
+  //             });
+  //           }
+  //           setSales(transformedSales);
+  //         }
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  if (error) {
+    return <p>Failed to load</p>;
   }
 
-  if (!sales) {
-    return <p>No data yet!</p>;
+  if (!data && !sales) {
+    return <p>Loading ...</p>;
   }
 
   return (
     <ul>
       {sales.map((sale) => (
-        <li key={sale.key}>
+        <li key={sale.id}>
           {sale.username} - ${sale.volume}
         </li>
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps(context) {
+  const salesURL = firebaseBase + "sales.json";
+  return fetch(salesURL)
+    .then((response) => response.json())
+    .then((data) => {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      return {
+        props: {
+          sales: transformedSales,
+        },
+        // revalidate: 10,
+      };
+    });
+  // return result;
 }
 
 export default LastSalesPage;
