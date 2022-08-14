6436f8ee49765d3f12414e31b08099992bb2761e
Anders Sollander <asolland@mathworks.com>
2022-08-14 20:54:48 +0200

    Add _document

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+// Used to customize the whole document
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+            <div id="overlay"></div>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
